07b12b5dc2fac3dbd1e154df4560770b16849a33
Tim Sengsouvanh <timsengsouvanh@gmail.com>
2021-03-18 18:08:43 +0900

    added menu items

--- src/home.js
+++ src/home.js
@@ -1,9 +1,16 @@
 function createAbout(id){
     const about = document.createElement('div')
     about.setAttribute('id', id)
-    about.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    
+
     return about
+}
+
+function createInformation(id){
+    const info = document.createElement('p')
+    info.setAttribute('id', id)
+    info.innerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    
+    return info
 }
 
 function loadHome(){
@@ -12,6 +19,9 @@ function loadHome(){
     
     const about = createAbout('about')
     main.appendChild(about)
+
+    const info = createInformation('about-info')
+    about.appendChild(info)
 }
 
 export default loadHome;
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,34 @@
 function createMenuArea(id){
     const menu = document.createElement('div')
     menu.setAttribute('id', id)
-    menu.innerText = 'Fish and Chips $1,000,000'
     
     return menu
+}
+
+function createMenuCategory(id, category){
+    const menuCategoryArea = document.createElement('div')
+    menuCategoryArea.setAttribute('id', id)
+    
+    const menuCategoryName = document.createElement('h1')
+    menuCategoryName.innerText = category
+    menuCategoryArea.appendChild(menuCategoryName)
+
+    return menuCategoryArea
+}
+
+function createMenuItem(id, name, price){
+    const menuItemArea = document.createElement('div')
+    menuItemArea.setAttribute('id', id)
+
+    const itemName = document.createElement('h2')
+    itemName.innerText = name
+    menuItemArea.appendChild(itemName)
+
+    const itemPrice = document.createElement('h2')
+    itemPrice.innerText = price
+    menuItemArea.appendChild(itemPrice)
+
+    return menuItemArea
 }
 
 function loadMenu(){
@@ -12,6 +37,27 @@ function loadMenu(){
     
     const menu = createMenuArea('menu-area')
     main.appendChild(menu)
+
+    const fish = createMenuCategory('menu-category', 'Fish')
+    menu.appendChild(fish)
+
+    const flake = createMenuItem('menuitem', 'flake', '6')
+    menu.appendChild(flake)
+
+    const barramundi = createMenuItem('menuitem', 'barramundi', '10')
+    menu.appendChild(barramundi)
+
+    const kingwhiting = createMenuItem('menuitem', 'king whiting', '12')
+    menu.appendChild(kingwhiting)
+
+    const chips = createMenuCategory('menu-category', 'Chips')
+    menu.appendChild(chips)
+
+    const minimumChips = createMenuItem('menuitem', 'minimum chips', '3')
+    menu.appendChild(minimumChips)
+
+    const potatoCake = createMenuItem('menuitem', 'potato cake', '1')
+    menu.appendChild(potatoCake)
 }
 
 export default loadMenu;
